be49844240ae2a32a9afabd0815aa1fe742c43bc
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-19 04:38:43 -0300

    ProfilePage

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+      <ProfileData>
+        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        <h1>Filipe Sales</h1>
+        <h2>@filipesalesaraujo</h2>
+        <p>Developer</p>
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 21 de novembro de 1995
+          </li>
+        </ul>
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>345 </strong> seguindo
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;

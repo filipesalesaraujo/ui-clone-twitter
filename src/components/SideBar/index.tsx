f88e1cd580be05fc4df46725b8b9bd47c7519517
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-27 17:33:43 -0300

    component sidebar

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+      <Body>
+        <p>{'lorem ipsum dolor sit amet. '.repeat(90)}</p>
+      </Body>
+    </Container>
+  );
+};
+
+export default SideBar;
